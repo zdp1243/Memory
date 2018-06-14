d12d4fb36abf9a9632f036debc30d806fc6a96b4
Jacinda Power <jjpower.writes@gmail.com>
2018-06-14 14:57:17 -0700

    add dependencies

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MemoryContainer from "./components/MemoryContainer";
+
+const App = () => <MemoryContainer />;
+
+export default App;
---
+++ src/components/clickcard/ClickCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./ClickCard.css";
+
+function handleClick(props) {
+  props.reArrangeCards();
+
+  props.clickedCharacter(props.id);
+}
+
+function ClickCard(props) {
+  return (
+    <div className="card img-container" onClick={() => handleClick(props)}>
+      <img alt={props.name} src={props.image} />
+    </div>
+  );
+}
